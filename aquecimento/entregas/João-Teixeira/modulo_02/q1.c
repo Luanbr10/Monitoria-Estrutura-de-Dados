ce67741d31b140f6cbfa419008950f873f28b0cd
João Victor Martins Teixeira <joao.victormt1210@gmail.com>
2025-10-07 17:48:35 +0000

    .

---
+++ aquecimento/entregas/João-Teixeira/modulo_02/q1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef _WIN32
+    #define LIMPAR_TELA "cls"
+#else
+    #define LIMPAR_TELA "clear"
+#endif
+
+void limpar_buffer(){
+    char c;
+    while((c = getchar()) != '\n' && c != EOF);
+}
+
+void main(){
+    int n;
+    
+    //vou considerar os meses tendo 30 dias sempre e anos tendo 365
+
+    printf("digite o numero:\n");
+    scanf("%d",&n);
+    printf("-------tabuada do %d-------\n",n);
+    for(int i = 1;i<=10;i++){
+        printf("%d x %d = %d\n",n,i,n*i);
+    }
+    printf("---------------------------\n");
+}
