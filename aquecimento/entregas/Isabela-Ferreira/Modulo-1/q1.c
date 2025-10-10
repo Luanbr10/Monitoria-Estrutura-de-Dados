cf507c70d7aca8e4a1f6ee7232268de6f1595e85
Isabela Arques <isabelaarkis@gmail.com>
2025-10-10 02:06:34 +0000

    .

---
+++ aquecimento/entregas/Isabela-Ferreira/Modulo-1/q1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){ 
+
+    int anos, meses, dias; 
+    int idade_dias;
+            
+    printf("Digite a idade em anos, meses e dias: ");
+    scanf("%d %d %d",anos, meses, dias);
+
+   idade_dias = anos * 365 + meses * 30 + dias;
+
+   printf("A idade dada em dias eh %d\n", idade_dias);
+
+   return 0;
+
+}
