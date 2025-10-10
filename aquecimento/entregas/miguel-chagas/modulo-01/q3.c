b20b1f55ecb855b8d1b7af4575c383906349289c
Miguel Chagas Maciel <miguelchagasmaciel@gmail.com>
2025-10-10 13:42:31 +0000

    salve

---
+++ aquecimento/entregas/miguel-chagas/modulo-01/q3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+
+    float raio;
+
+    printf("Digite o raio do seu círculo: ");
+    scanf("%f",&raio);
+
+    printf("O diâmetro do seu círculo então vai ser: %.2f\n",raio * 2);
+    printf("Sendo assim, a área dele é: %.2f\n",(raio * raio) * 3.14);
+    printf("E então seu diâmetro é: %.2f\n",raio * 2 * 3.14);
+
+    return 0;
+}
