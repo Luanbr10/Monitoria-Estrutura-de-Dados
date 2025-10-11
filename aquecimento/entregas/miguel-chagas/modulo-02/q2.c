0f7ed76ad4725449b79b642a76b161380157497f
Miguel Chagas Maciel <miguelchagasmaciel@gmail.com>
2025-10-11 02:16:09 +0000

    q2 mod2

--- aquecimento/entregas/miguel-chagas/modulo-02/q2.c
+++ aquecimento/entregas/miguel-chagas/modulo-02/q2.c
@@ -3,28 +3,31 @@
 
 int main(){
     float notas[5];
-    float media,aux;
+    float media = 0.0;
+    float aux;
 
     printf("Digite 5 notas: \n");
     
     for(int i = 0; i < 5; i++){
         printf("> ");
         scanf("%f",&notas[i]);
-        media += notas[i] ;
+        media += notas[i];
     }
 
+    media = media / 5;
+
     for(int i = 0;i < 5; i++){
-        for(int j = 0;j < 5; j++){
-            if(notas[j] > notas[j + 1]){
-                aux = notas[j];
-                notas[j] = notas[j+1];
-                notas[j+1] = j;
+        for(int j = 1;j < 5; j++){
+            if(notas[j] < notas[j - 1]){
+                aux = notas[j - 1];
+                notas[j - 1] = notas[j];
+                notas[j] = aux;
             }
         }
     }
 
-    for(int i = 0; i < 5; i++){
-        printf("%d - %.2f\n",i,notas[i]);
-    }
+    printf("A maior nota é: %.2f\n",notas[4]);
+    printf("A menor nota é: %.2f\n",notas[0]);
+    printf("E a média é: %.2f\n",media);
 
 }
